0a79136096957afed6ed5d3a9fbf00a06d680e6d
termdeveloper <103795291+termdeveloper@users.noreply.github.com>
2022-04-16 09:30:13 +0700

    Love animation!.py
    Python la

---
+++ Game.py
@@ -0,0 +1,14 @@
+from gapminder import gapminder
+
+gapminder.head()
+country continent  year  lifeExp       pop   gdpPercap
+
+0  Love    bbb      Asia  1952   28.801   8425333  779.445314
+
+1  Love forever     Asia  1957   30.332   9240934  820.853030
+
+2  Love you for entity      Asia  1962   31.997  10267083  853.100710
+
+3  Create magic love      Asia  1967   34.020  11537966  836.197138
+
+4  Loveeeeeeeeeeee
